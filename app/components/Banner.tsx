9348bf26c058ac91a25012d0c8163b966012cd84
aditya <aditya.ramariya@gmail.com>
2026-08-17 14:39:55 +0530

    fix: fix .env.local and .env.production to make the absoute path to relative path for videos and images

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -15,7 +15,11 @@ const Banner = () => {
           preload="auto"
           className="absolute inset-0 h-full w-full object-cover"
         >
-          <source src="/videos/banner-video.mp4" type="video/mp4" />
+          {/* <source src="/videos/banner-video.mp4" type="video/mp4" /> */}
+          <source
+            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/videos/banner-video.mp4`}
+            type="video/mp4"
+          />
         </video>
       </div>
       {/* =========================
