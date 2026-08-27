06d5be32c5312f003995514cb4d36e883d0ffee2
aditya <aditya.ramariya@gmail.com>
2026-08-27 22:05:40 +0530

    fix: baseURL for development mode and production

--- constant.tsx
+++ constant.tsx
@@ -1,3 +1,7 @@
 export const basePath =
   process.env.NODE_ENV === "production" ? "/portfolio" : "";
-export const BaseURL = "https://localhost:7104";
+// export const BaseURL = "https://localhost:7104";
+export const BaseURL =
+  process.env.NODE_ENV === "development"
+    ? "https://localhost:7104"
+    : "https://portfolio-backend-yfqp.onrender.com";
